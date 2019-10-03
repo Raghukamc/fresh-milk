8b6efc27d7c37c1972dc448102578580c58a7199
Johnson Sequeira <eminem1316@gmail.com>
2019-10-03 21:55:30 +0530

    Updates

---
+++ precache-manifest.7ec3e1c54941b6a7e70c5e3deab5d6b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f821c1f7df2edb9400d63a97ddc29032",
+    "url": "/Raghukamc/fresh-milk.git/index.html"
+  },
+  {
+    "revision": "76aa9605db7fdc046606",
+    "url": "/Raghukamc/fresh-milk.git/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "76aa9605db7fdc046606",
+    "url": "/Raghukamc/fresh-milk.git/static/js/2.f1b5d57d.chunk.js"
+  },
+  {
+    "revision": "92a1dcf41a134007aaa1",
+    "url": "/Raghukamc/fresh-milk.git/static/js/main.db29366b.chunk.js"
+  },
+  {
+    "revision": "f7edf94c4df8d5b9200a",
+    "url": "/Raghukamc/fresh-milk.git/static/js/runtime-main.28348068.js"
+  },
+  {
+    "revision": "d7e5b3b0dc36d910dd321cbaad2f7b03",
+    "url": "/Raghukamc/fresh-milk.git/static/media/banner1.d7e5b3b0.jpg"
+  },
+  {
+    "revision": "09dd4fcf5aeb785c2f546ab797e89b53",
+    "url": "/Raghukamc/fresh-milk.git/static/media/banner2.09dd4fcf.jpg"
+  },
+  {
+    "revision": "f5c6da7fe960ebd9aa92ff1deecbdd00",
+    "url": "/Raghukamc/fresh-milk.git/static/media/banner3.f5c6da7f.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Raghukamc/fresh-milk.git/precache-manifest.7ec3e1c54941b6a7e70c5e3deab5d6b9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Raghukamc/fresh-milk.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
